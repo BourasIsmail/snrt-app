992e7eed31e26b07d6f7ff7b82d24ef4ea856519
ismail <ismailbouras1968@gmail.com>
2025-04-03 12:29:29 +0200

    ini

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,3 +1,10 @@
+-- Create the unite table
+CREATE TABLE unite (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(100) NOT NULL,
+    description VARCHAR(200)
+);
+
 -- Create the roles table
 CREATE TABLE roles (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -6,7 +13,9 @@ CREATE TABLE roles (
 
 
 
+
 -- Insert the SUPER_ADMIN_ROLES role
 INSERT INTO roles (role) VALUES ('SUPER_ADMIN_ROLES');
 
+INSERT INTO unite (nom,description) VALUES ('HD3', 'Description unite HD3')
 
